fe1a304b81d19bcbe90154e23bcd9f6f6bec7193
Anton Efimenko <reloni@ya.ru>
2020-01-02 01:05:40 +0300

    Fix ingress config

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sh deploy-app.sh goexample $CI_COMMIT_REF_NAME ./apps/gotest-app.yaml
-sh deploy-app.sh goexample-redis $CI_COMMIT_REF_NAME ./apps/gotest-redis.yaml
-cat ./apps/ingress-dev.yaml | envsubst | kubectl apply -f -
+sh deploy-app.sh goexample $CI_COMMIT_REF_NAME ./apps/gotest-app.yaml && \
+  sh deploy-app.sh goexample-redis $CI_COMMIT_REF_NAME ./apps/gotest-redis.yaml && \
+  cat ./apps/ingress-dev.yaml | envsubst | kubectl apply -f -
