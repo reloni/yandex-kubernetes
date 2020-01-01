7075ebe870620e28fdbebdacc66374a112562bfb
Anton Efimenko <reloni@ya.ru>
2020-01-01 04:59:24 +0300

    use envsubst

--- deploy-gotest.sh
+++ deploy-gotest.sh
@@ -4,35 +4,34 @@ set -e
 docker pull reloni/goexample:$CI_COMMIT_REF_NAME 2> /dev/null || true
 
 if [[ "$(docker images -q reloni/goexample:$CI_COMMIT_REF_NAME 2> /dev/null)" == "" ]]; then
-  TAG=latest
+  EXPORT GOTEST_TAG=latest
 else
-  TAG=$CI_COMMIT_REF_NAME
+  EXPORT GOTEST_TAG=$CI_COMMIT_REF_NAME
 fi
 
-TAG=latest
+# cat <<EOF > gotest-deployment.yaml
+# apiVersion: apps/v1
+# kind: Deployment
+# metadata:
+#   name: gotest-app
+# spec:
+#   replicas: 5
+#   selector:
+#     matchLabels:
+#       app: gotest-app
+#   template:
+#     metadata:
+#       labels:
+#         app: gotest-app
+#     spec:
+#       containers:
+#       - image: reloni/goexample:latest
+#         name: gotest-app
+#         ports:
+#         - containerPort: 8080
+#           protocol: TCP
+# EOF
 
-cat <<EOF > gotest-deployment.yaml
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: gotest-app
-spec:
-  replicas: 5
-  selector:
-    matchLabels:
-      app: gotest-app
-  template:
-    metadata:
-      labels:
-        app: gotest-app
-    spec:
-      containers:
-      - image: reloni/goexample:latest
-        name: gotest-app
-        ports:
-        - containerPort: 8080
-          protocol: TCP
-EOF
-
-kubectl apply -f gotest-deployment.yaml
-rm gotest-deployment.yaml
+# kubectl apply -f gotest-deployment.yaml
+# rm gotest-deployment.yaml
+cat gotest-app.yaml | envsubst | kubectl apply
