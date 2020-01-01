798429af08c7ae9716e7b3e1021769186e7cf37b
Anton Efimenko <reloni@ya.ru>
2020-01-02 01:08:48 +0300

    Update script

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-sh deploy-app.sh goexample $CI_COMMIT_REF_NAME ./apps/gotest-app.yaml && \
-  sh deploy-app.sh goexample-redis $CI_COMMIT_REF_NAME ./apps/gotest-redis.yaml && \
-  cat ./apps/ingress-dev.yaml | envsubst | kubectl apply -f -
+echo "Deploy gotest"
+sh deploy-app.sh goexample $CI_COMMIT_REF_NAME ./apps/gotest-app.yaml
+
+echo "Deploy redis"
+sh deploy-app.sh goexample-redis $CI_COMMIT_REF_NAME ./apps/gotest-redis.yaml
+
+echo "Update ingress"
+cat ./apps/ingress-dev.yaml | envsubst | kubectl apply -f -
