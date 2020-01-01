ee2ea98f08a609578ecfd257e12a9e3d9230ccc4
Anton Efimenko <reloni@ya.ru>
2020-01-01 05:12:58 +0300

    Fix script

--- deploy-gotest.sh
+++ deploy-gotest.sh
@@ -3,35 +3,10 @@ set -e
 
 docker pull reloni/goexample:$CI_COMMIT_REF_NAME 2> /dev/null || true
 
-if [[ "$(docker images -q reloni/goexample:$CI_COMMIT_REF_NAME 2> /dev/null)" == "" ]]; then
-  EXPORT GOTEST_TAG=latest
+if ["$(docker images -q reloni/goexample:$CI_COMMIT_REF_NAME 2> /dev/null)" == ""]; then
+  export GOTEST_TAG=latest
 else
-  EXPORT GOTEST_TAG=$CI_COMMIT_REF_NAME
+  export GOTEST_TAG=$CI_COMMIT_REF_NAME
 fi
 
-# cat <<EOF > gotest-deployment.yaml
-# apiVersion: apps/v1
-# kind: Deployment
-# metadata:
-#   name: gotest-app
-# spec:
-#   replicas: 5
-#   selector:
-#     matchLabels:
-#       app: gotest-app
-#   template:
-#     metadata:
-#       labels:
-#         app: gotest-app
-#     spec:
-#       containers:
-#       - image: reloni/goexample:latest
-#         name: gotest-app
-#         ports:
-#         - containerPort: 8080
-#           protocol: TCP
-# EOF
-
-# kubectl apply -f gotest-deployment.yaml
-# rm gotest-deployment.yaml
-cat gotest-app.yaml | envsubst | kubectl apply
+cat gotest-app.yaml | envsubst | kubectl apply -f -
