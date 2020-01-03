10ab70c74e886fd28680c5aa0b5f5aa806753bc1
Anton Efimenko <reloni@ya.ru>
2020-01-03 16:47:39 +0300

    Fix again

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -12,7 +12,7 @@ sh deploy-app.sh goexample-redis $CI_COMMIT_REF_NAME ./apps/gotest-redis.yaml
 echo "Update ingress"
 kubectl delete ingress --all --namespace=$CI_COMMIT_REF_NAME
 
-NAMESPACE='$CI_COMMIT_REF_NAME' \
+NAMESPACE="$CI_COMMIT_REF_NAME" \
   envsubst '$NAMESPACE' < ./apps/ingress-dev.yaml \
   | kubectl apply -f -
 cat ./nginx-ingress-controller/nginx-config.yaml | kubectl apply -f -
